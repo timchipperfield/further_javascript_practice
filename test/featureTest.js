60c89271a954ed962edc18e25b055f1a31612b02
timchipperfield <timchipperfield@gmail.com>
2016-07-06 12:32:41 +0100

    added first web feature test - working

--- test/featureTest.js
+++ test/featureTest.js
@@ -1,16 +1,20 @@
+var app = require("http-server").createServer();
+var assert = require('assert');
 var Browser = require('zombie');
-var createServer = require('http-server').createServer;
-var path = require('path');
-var server = createServer({root: path.join(__dirname, "../")});
+describe('title page', function() {
+  before(function() {
+    server = app.listen(3000);
+    browser = new Browser({ site: 'http://localhost:3000' });
+  });
+  beforeEach(function(done) {
+    browser.visit('/', done);
+  });
 
-server.listen(3000);
-Browser.localhost('localhost
-', 3000);
+  it('is findable', function() {
+    browser.assert.status(200);
+  });
 
-describe('user visits page', function() {
-  var browser = new Browser();
-
-  it('should have a title', function() {
-    browser.assert.text('title', 'To Do List');
+  it('displays a title', function() {
+    browser.assert.text('h1', 'To Do List');
   });
 });
